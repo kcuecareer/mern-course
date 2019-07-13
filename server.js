a3141a5725055464e2221222743c7798dcf0bdb0
Kathy Cuevas <meetnerdykat@gmail.com>
2019-07-13 14:56:51 -0400

    Finally figured out the error. I named the default file with an extension of dot js when it should be dot json

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require('./config/db');
 
+
+// Created variable to hold Express
 const app = express();
+
+// Connect the database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Server API is up and running'));
+app.get("/", (req, res) => res.send("Server API is up and running"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
